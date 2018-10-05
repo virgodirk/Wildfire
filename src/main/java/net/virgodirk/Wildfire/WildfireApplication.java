bfb93951fcf7ca692a9df1550c73d70600dc16af
virgodirk <lixiaoyongyx@163.com>
2018-10-05 18:46:46 +0800

    创建

---
+++ src/main/java/net/virgodirk/Wildfire/WildfireApplication.java
@@ -0,0 +1,12 @@
+package net.virgodirk.Wildfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildfireApplication.class, args);
+	}
+}
---
+++ src/test/java/net/virgodirk/Wildfire/WildfireApplicationTests.java
@@ -0,0 +1,16 @@
+package net.virgodirk.Wildfire;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WildfireApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
